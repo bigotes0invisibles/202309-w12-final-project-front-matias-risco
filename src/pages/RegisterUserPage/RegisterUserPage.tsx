a4768ca5af269a4efd90936f5caa0ef44bac4e22
Matias Risco <matiasriscolagos@gmail.com>
2024-02-27 19:12:53 +0100

    Add Page RegisterUserPage

---
+++ src/pages/RegisterUserPage/RegisterUserPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterUserPageStyled from "./RegisterUserPageStyled";
+
+const RegisterUserPage = (): React.ReactElement => {
+  return (
+    <RegisterUserPageStyled>
+      <h1>Register</h1>
+    </RegisterUserPageStyled>
+  );
+};
+
+export default RegisterUserPage;
---
+++ src/pages/RegisterUserPage/RegisterUserPageStyled.ts
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+const RegisterUserPageStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  padding: 15px 0 134px;
+
+  h1 {
+    height: 51px;
+    width: 180px;
+    background-color: ${({ theme }) => theme.color.containerBackground};
+    color: ${({ theme }) => theme.color.secondaryFont};
+    text-align: center;
+    align-self: center;
+    padding: 6px 15px;
+    font-size: ${({ theme }) => theme.typography.titleSize};
+  }
+`;
+
+export default RegisterUserPageStyled;
